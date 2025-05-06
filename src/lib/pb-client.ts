a5fa6b702d50b0f990e4bf65e149bdbbd30d395d
Jaynel Patiarba <lenyajpatiarba@gmail.com>
2025-05-06 15:48:58 +0800

    production

--- src/lib/pb-client.ts
+++ src/lib/pb-client.ts
@@ -2,7 +2,7 @@
 import PocketBase from 'pocketbase';
 
 // Create a PocketBase client instance
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('https://pocket.jwisnetwork.com');
 
 export interface YouthRecord {
   id: string;
